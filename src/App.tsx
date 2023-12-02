1ecf45175061bcd93f639de128f7992670c6659b
Snaehath <2020ad0556@svce.ac.in>
2023-12-02 19:31:15 +0530

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,24 @@ export default function App(){
         { id: crypto.randomUUID(), title: newItem, completed: false},
       ]
     })
-      setNewItem("")
+    setNewItem("")
+  }
+  function toggleTodo(id:string, completed:boolean){
+    setTodos(currentTodos =>{
+      return currentTodos.map(todo =>{
+        if(todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
   }
 
+  function deleteTodo(id:string){
+    setTodos(currentTodos =>{
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -31,15 +46,16 @@ export default function App(){
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return(
             <li key={todo.id}>
               <label>
-              <input type="checkbox" checked={todo.completed}/>
+              <input type="checkbox" checked={todo.completed}
+              onChange={e => toggleTodo(todo.id,e.target.checked)}/>
               {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
             </li>
           )
         })}
