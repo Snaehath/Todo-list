32d91fe3a40352e391704b857e97363a866969a2
Snaehath <2020ad0556@svce.ac.in>
2023-12-03 13:55:33 +0530

    updated deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  base: "/Todo-list/",
+  build: {
+    outDir: "build"
+  },
+  server: {
     open: true
   }
 })
